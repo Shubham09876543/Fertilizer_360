fb2e5c9e97906d1adb89818f1a0bf35de5502f57
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-04-04 21:44:04 +0530

    Add About us page with Working css

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./frontend/home_page.jsx";
 import ShopPage from "./frontend/ShopPage.jsx";
 import ContactUs from "./frontend/contactus_page.jsx";
+import About from "./frontend/About.jsx";
 // import AdminDashboard from "./Admin/Admin_dash.jsx"; // Ensure correct path
 // import "./index.css"; // Ensure styles are included
 
@@ -21,10 +22,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/shop" element={<ShopPage />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/shop/:id" element={<ShopDetails />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/shop/:id" element={<ShopDetails />} />
         <Route path="/fertilizer/:id" element={<FertilizerDetails />} />
-        <Route path="/fertilizer/:id/advance_booking" element={<AdvanceBooking />} /> */}
+        <Route path="/fertilizer/:id/advance_booking" element={<AdvanceBooking />} />  */}
         {/* <Route path="/profile" element={<ProfilePage />} />  */}
       </Routes>
     </Router>

1c6d36a4fe659fbaa3dd24103fb511aa16c6662e
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-04-15 19:08:17 +0530

    Some chnage in home page make button work

--- src/frontend/home_page.jsx
+++ src/frontend/home_page.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from "./navbar";
 import Footer from "./footer";
 import "./CSS/HomePage.css";
 
 const HomePage = () => {
+  const navigate = useNavigate();
   const images = [
     "https://www.foodicine.co.in/img/products/humic-acid-fertilizer.jpg",
     "https://4.imimg.com/data4/JB/PJ/MY-26160485/stim-rich-500x500.jpg",
@@ -22,14 +24,17 @@ const HomePage = () => {
     );
   };
 
+  const goToShop = () => {
+    navigate("/shop");
+  };
+
   return (
     <div className="home-container">
       <Navbar />
-
       {/* Hero Section */}
       <section className="hero-section">
         <img
-          src="https://t4.ftcdn.net/jpg/02/44/02/17/360_F_244021725_GsYLozplO0VH7oI5ONClebHBAN8RiHbo.jpg"
+          src="https://media.licdn.com/dms/image/v2/D5610AQEOVcHd-KCQ1w/image-shrink_800/image-shrink_800/0/1734719281602?e=2147483647&v=beta&t=ttFOxJr_YEcrdGW8DkYNdPqdPUcOpsByqKPzI5rlEjI"
           alt="Hero"
           className="hero-image"
         />
@@ -38,7 +43,7 @@ const HomePage = () => {
             Discover nearby shops with the fertilizer stocks you need.
           </p>
           <p className="hero-subtext">Stay current with the latest updates.</p>
-          <button className="hero-button">CHECK NOW!</button>
+          <button className="hero-button" onClick={goToShop}>CHECK NOW!</button>
         </div>
       </section>
 
@@ -68,7 +73,7 @@ const HomePage = () => {
           <p>
             Discover a wide range of fertilizers, complete with prices and stock availability in various shops.
           </p>
-          <button className="explore-button">Explore More</button>
+          <button className="explore-button" onClick={goToShop}>Explore More</button>
         </div>
 
         {/* Image Slider */}
